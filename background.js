46b00e0ff1615f4c380a8e0a43641cc7c5439314
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-10-07 18:02:05 +0300

    gmail ile giriş
    gmaili sonra kendi sistemimizle değiştirirm, api anahtarı alıp, gmail ile giriş de kalabilir mobilde işe yarar bence

--- background.js
+++ background.js
@@ -1,53 +1,9 @@
-const DISCORD_URI_ENDPOINT = 'https://app.securityforeveryone.com/sign-in';
-const CLIENT_ID = encodeURIComponent('');
-const RESPONSE_TYPE = encodeURIComponent('token');
-const REDIRECT_URI = encodeURIComponent('');
-const SCOPE = encodeURIComponent('identify email');
-const STATE = encodeURIComponent('meet' + Math.random().toString(36).substring(2, 15));
-
-let user_signed_in = false;
-
-function create_auth_endpoint() {
-    let nonce = encodeURIComponent(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
-
-    let endpoint_url =
-        `${securityforeveryone_URI_ENDPOINT}
-?client_id=${CLIENT_ID}
-&redirect_uri=${REDIRECT_URI}
-&response_type=${RESPONSE_TYPE}
-&scope=${SCOPE}
-&nonce=${nonce}`;
-
-    return endpoint_url;
-}
-
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'login') {
-        chrome.identity.launchWebAuthFlow({
-            url: create_auth_endpoint(),
-            interactive: true
-        }, function (redirect_uri) {
-            if (chrome.runtime.lastError || redirect_uri.includes('access_denied')) {
-                console.log("Could not authenticate.");
-                sendResponse('fail');
-            } else {
-                user_signed_in = true;
-                sendResponse('success');
-            }
-        });
-
-        return true;
-    } else if (request.message === 'logout') {
-        user_signed_in = false;
-
-        sendResponse('success');
-    }
-});
-
-
-
-
+let window = self;
+try {
+  importScripts('./jsrsasign-all-min.js');
+} catch (e) {
+  console.log(e);
+};
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension have been successfully installed!");
   });
