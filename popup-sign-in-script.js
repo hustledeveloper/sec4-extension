88f6c61d19d79c760e94339f97cafbf9c3863dcd
Ömer Faruk Eren <101640251+omer832@users.noreply.github.com>
2022-11-02 15:25:36 +0300

    Update popup-sign-in-script.js

--- popup-sign-in-script.js
+++ popup-sign-in-script.js
@@ -1,32 +1,33 @@
-const giris_buton = document.querySelector('#gir');
+const giris_buton = document.querySelector("#gir");
 
-giris_buton.addEventListener('mouseover', () => {
-    giris_buton.style.backgroundColor = 'black';
-    giris_buton.style.color = 'white';
-    giris_buton.style.transform = 'scale(1.3)';
+giris_buton.addEventListener("mouseover", () => {
+  giris_buton.style.backgroundColor = "black";
+  giris_buton.style.color = "white";
+  giris_buton.style.transform = "scale(1.3)";
 });
 
-giris_buton.addEventListener('mouseleave', () => {
-    giris_buton.style.backgroundColor = '#ee2c1e';
-    giris_buton.style.color = 'white';
-    giris_buton.style.transform = 'scale(1)';
+giris_buton.addEventListener("mouseleave", () => {
+  giris_buton.style.backgroundColor = "#ee2c1e";
+  giris_buton.style.color = "white";
+  giris_buton.style.transform = "scale(1)";
 });
-document.querySelector('form').addEventListener('submit', event => {
-    event.preventDefault();
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const email = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-    const apitoken = document.querySelector('#api-token').value;
+  const email = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  const apitoken = document.querySelector("#api-token").value;
 
-    if (email && password) {
-        chrome.runtime.sendMessage({ message: 'login', 
-      payload: { email, password, apitoken }},
+  if (email && password) {
+    chrome.runtime.sendMessage(
+      { message: "login", payload: { email, password, apitoken } },
       function (response) {
-          if (response === 'success')
-              window.location.replace('./popup-sign-out.html');
-  });
-    } else {
-        document.querySelector('#username').placeholder = "Enter a username.";
-        document.querySelector('#password').placeholder = "Enter a password.";
-    }
+        if (response === "success")
+          window.location.replace("./popup-sign-out.html");
+      }
+    );
+  } else {
+    document.querySelector("#username").placeholder = "Enter a username.";
+    document.querySelector("#password").placeholder = "Enter a password.";
+  }
 });
