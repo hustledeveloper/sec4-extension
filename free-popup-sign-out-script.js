50a97e56bb2dbe40bd6c0d3444abe2d6849d05fe
omer832 <faruk008887@gmail.com>
2022-12-15 20:16:47 +0300

    autocomp

--- free-popup-sign-out-script.js
+++ free-popup-sign-out-script.js
@@ -1,32 +1,3 @@
-const search = document.querySelector("#scantipi");
-//SEARCH BÖLÜMÜ freetolls dan veri arayacak şekilde ayarla
-const todos = document.querySelectorAll("ul li");
-const notFound = document.querySelector("#notFound");
-
-search.addEventListener("keyup", filterFunctionality);
-
-function filterFunctionality(e) {
-  let searching = e.target.value.toLowerCase();
-
-  // Filters the search
-  [...todos].forEach((todo) => {
-    let todoContent = todo.textContent;
-    if (todoContent.toLowerCase().includes(searching)) {
-      todo.style.display = "block";
-    } else {
-      todo.style.display = "none";
-    }
-  });
-
-  // Displays No search Found
-  let result = [...todos].every((todo) => {
-    return todo.style.display === "none";
-  });
-
-  result === true
-    ? (notFound.style.display = "block")
-    : (notFound.style.display = "none");
-}
 //TASARIM
 const cikis_buton = document.querySelector(".cikis");
 
