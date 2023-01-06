e1dd4c6a006a01fbb15deb5ce78dbb5dc6c84425
omer832 <faruk008887@gmail.com>
2023-01-06 23:35:43 +0300

    Update popup-sign-out-script.js

--- popup-sign-out-script.js
+++ popup-sign-out-script.js
@@ -1,10 +1,14 @@
+const cikis_buton = document.querySelector(".cikis");
+const reset_asset_buton = document.querySelector(".reset-token");
+const scan_butonu = document.querySelector(".scan");
+const change_asset_buton = document.querySelector(".change-asset");
+
 //SCAN CLICK düzenlemeler yapılacak
 scan_butonu.addEventListener("click", () => {
   chrome.runtime.sendMessage("start_scan", (response) => {
     console.log(response);
   });
 });
-
 //asseti gösterme
 chrome.storage.local.get("asseturl", function (result) {
   const myValue = result.asseturl;
@@ -19,7 +23,6 @@ chrome.storage.local.get("scan_aktive", function (result) {
 });
 
 //TASARIM
-const reset_asset_buton = document.querySelector(".reset-token");
 
 reset_asset_buton.addEventListener("mouseover", () => {
   reset_asset_buton.style.backgroundColor = "black";
@@ -39,7 +42,6 @@ reset_asset_buton.addEventListener("click", () => {
     chrome.storage.local.set({ asseturl: asseturl }).then(() => {});
   });
 });
-const cikis_buton = document.querySelector(".cikis");
 
 cikis_buton.addEventListener("mouseover", () => {
   cikis_buton.style.backgroundColor = "black";
@@ -64,7 +66,6 @@ cikis_buton.addEventListener("click", () => {
   window.location.replace("./popup-sign-in.html");
 });
 
-const scan_butonu = document.querySelector(".scan");
 //TASARIM
 scan_butonu.addEventListener("mouseover", () => {
   scan_butonu.style.backgroundColor = "black";
@@ -77,8 +78,6 @@ scan_butonu.addEventListener("mouseleave", () => {
   scan_butonu.style.color = "white";
   scan_butonu.style.transform = "scale(1)";
 });
-
-const change_asset_buton = document.querySelector(".change-asset");
 
 change_asset_buton.addEventListener("mouseover", () => {
   change_asset_buton.style.backgroundColor = "black";
