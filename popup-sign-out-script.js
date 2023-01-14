8fbe9226a0b6fc4af6bc04907070a2183b09ee57
omer832 <faruk008887@gmail.com>
2023-01-14 21:18:24 +0300

    Update popup-sign-out-script.js

--- popup-sign-out-script.js
+++ popup-sign-out-script.js
@@ -4,9 +4,7 @@ const scan_butonu = document.querySelector(".scan");
 const change_asset_buton = document.querySelector(".change-asset");
 //SCAN CLICK düzenlemeler yapılacak
 scan_butonu.addEventListener("click", () => {
-  chrome.runtime.sendMessage("start_scan", (response) => {
-    console.log(response);
-  });
+  chrome.runtime.sendMessage("start_scan", (response) => {});
 });
 //asseti gösterme
 chrome.storage.local.get("asseturl", function (result) {
