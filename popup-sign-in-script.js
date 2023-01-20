7f90434149b5f81d9156e3f95c103a3b1112ea16
omer832 <faruk008887@gmail.com>
2023-01-20 19:48:08 +0300

    better login
    uyarılar ve kontroller eklendi

--- popup-sign-in-script.js
+++ popup-sign-in-script.js
@@ -14,9 +14,36 @@ giris_buton.addEventListener("mouseleave", () => {
 
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-
   let apitoken = document.querySelector("#api-token").value;
   //apitoken localde kaydedildi
-  chrome.storage.local.set({ apitoken: apitoken }).then(() => {});
-  window.location.replace("./popup-welcome.html");
+  isValidToken(apitoken);
 });
+function isValidToken(mytoken) {
+  if (mytoken === undefined || mytoken === null) {
+    document.getElementById("warning-message").style.display = "block";
+    return;
+  }
+  if (mytoken.length < 32) {
+    document.getElementById("warning-message").innerHTML =
+      "Token 32 karakterden küçük olamaz";
+    document.getElementById("warning-message").style.display = "block";
+    return;
+  }
+  if (mytoken.length > 256) {
+    document.getElementById("warning-message").innerHTML =
+      "Token 256 karakterden büyük olamaz";
+    document.getElementById("warning-message").style.display = "block";
+    return;
+  }
+  const valid = /^[a-zA-Z0-9_\-]*$/.test(mytoken);
+  if (!valid) {
+    document.getElementById("warning-message").innerHTML =
+      "Token geçerli karakterler içermelidir";
+    document.getElementById("warning-message").style.display = "block";
+  }
+  if (valid) {
+    let apitoken = document.querySelector("#api-token").value;
+    chrome.storage.local.set({ apitoken: apitoken }).then(() => {});
+    window.location.replace("./popup-welcome.html");
+  }
+}
